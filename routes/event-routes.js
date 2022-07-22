4e522beb64bd420b93dad10adbb7f255d0e2cc7d
ahmed-fareeq-0 <ahmed.fareeq.0@gmail.com>
2022-07-23 02:58:55 +0300

    create route and files ejs

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
+// route to home event
 router.get("/", (req, res) => {
     res.render("event/index.ejs")
+})
+
+// show single event
+router.get("/:id", (req, res) => {
+    res.render("event/showEvent.ejs")
 })
 
 module.exports = router
